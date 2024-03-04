9f9a122525b07c916adf728d815e0cc4c31cdbd5
Diego <you@example.com>
2024-03-04 09:24:35 +0100

    ATM Fertig

--- Bankomat.py
+++ Bankomat.py
@@ -12,14 +12,29 @@ while True:
     print("4.Beenden")
     print(" ")
 
-    Userinput = input("Wählen Sie ihre Aktion aus")
+    Userinput = input("Wählen Sie ihre Aktion aus: ")
     print("-------------------------")
 
     if Userinput == "1":
         print("Geben Sie das gewollte Geld ein")
-        betrag = int(input("Geldmenge"))
-        Kontostand += betrag
-        print("Sie haben x € eingezahlt.")
-    
+        Ebetrag = int(input("Gewünschter Betrag: "))
+        Kontostand += Ebetrag
+        print("Sie haben", Ebetrag, "€ Eingezahlt.")
+        print("Ihr Kontostand beträgt:",Kontostand)
+        print("-------------------------")
 
-   
+    if Userinput == "2":
+        print("Wie viel möchten Sie abheben?")
+        Abetrag = int(input("Gewünschter Betrag: "))
+        Kontostand -= Abetrag
+        print("Sie haben", Abetrag, "€ Eingezahlt.")
+        print("Ihr Kontostand beträgt:",Kontostand)
+        print("-------------------------")
+
+    if Userinput == "3":
+        print("Ihr Kontostand beträgt:",Kontostand)
+        print("-------------------------")
+
+    if Userinput == "4":
+        print("Entnehmen Sie ihre.")
+        break
